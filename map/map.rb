33be886bb2587691917b4cf6bcf4edf58ccc8c5e
David Yeung <yeungda@gmail.com>
2011-06-28 18:36:08 +1000

    roles aren't working out. trying map instead

---
+++ map/map.rb
@@ -0,0 +1,104 @@
+location :start do
+  to :log_in do |browser, data|
+    browser.navigate.to data[:url]
+  end
+
+  to :home do |browser, data|
+    browser.navigate.to data[:url]
+  end
+end
+
+location :home do
+  to :articles do |browser, data|
+    browser.find_element(:link_text => 'Articles').click
+  end
+end
+
+location :articles do
+
+end
+
+location :log_in do
+  to :log_in_failed do |browser, data|
+    browser.find_element(:id => 'admin_user_email').send_keys data[:email_address]
+    browser.find_element(:id => 'admin_user_password').send_keys data[:password]
+    browser.find_element(:id => 'admin_user_submit').click
+  end
+
+  to :log_in_successful do |browser, data|
+    browser.find_element(:id => 'admin_user_email').send_keys data[:email_address]
+    browser.find_element(:id => 'admin_user_password').send_keys data[:password]
+    browser.find_element(:id => 'admin_user_submit').click
+  end
+
+  observations do |browser|
+    {:page_title => browser.title}
+  end
+end
+
+location :log_in_failed do
+  observations do |browser|
+    {
+      :page_title => browser.title,
+      :message => browser.find_element(:css => '.flash_alert').text
+    }
+  end
+end
+
+location :log_in_successful do
+  to :dashboard do end
+
+  observations do |browser|
+    {:page_title => browser.title}
+  end
+end
+
+location :dashboard do
+  to :admin_articles do |browser, data|
+    browser.find_element(:link_text => 'Articles').click
+  end
+end
+
+location :admin_articles do
+
+  to :new_article do |browser, data|
+    browser.find_element(:link_text => 'New Article').click
+  end
+
+  observations do |browser|
+    {
+      :articles => browser.find_element(:css => 'table#articles').text
+    }
+  end
+end
+
+location :new_article do
+
+  to :successfully_created_article do |browser, data|
+    browser.find_element(:id => 'article_title').send_keys data[:article_title]
+    browser.find_element(:id => 'article_body').send_keys data[:article_body]
+    browser.find_element(:id => 'article_submit').click
+  end
+
+  to :unsuccessfully_created_article do |browser, data|
+    browser.find_element(:id => 'article_title').send_keys data[:article_title]
+    browser.find_element(:id => 'article_body').send_keys data[:article_body]
+    browser.find_element(:id => 'article_submit').click
+  end
+end
+
+location :successfully_created_article do
+  to :admin_articles do |browser, data|
+    browser.find_element(:link_text => 'Articles').click
+  end
+
+  observations do |browser|
+    {
+      :article => browser.find_element(:css => '.attributes_table.article').text
+    }
+  end
+end
+
+location :unsuccessfully_created_article do 
+  
+end
--- test.rb
+++ test.rb
@@ -1,5 +1,5 @@
 Dir[File.dirname(__FILE__) + "/lib/*.rb"].each {|file| require file }
-Dir[File.dirname(__FILE__) + "/roles/*.rb"].each {|file| require file }
+Dir[File.dirname(__FILE__) + "/map/*.rb"].each {|file| require file }
 Dir[File.dirname(__FILE__) + "/users/*.rb"].each {|file| require file }
 Dir[File.dirname(__FILE__) + "/scenarios/*.rb"].each {|file| require file }
 
