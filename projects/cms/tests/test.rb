e80cafba75e11c56aed5e14612d787e29a6b969e
David Yeung <yeungda@gmail.com>
2012-02-23 08:44:03 +1100

    renamed map to states and scenarios to journeys

--- projects/cms/tests/test.rb
+++ projects/cms/tests/test.rb
@@ -1,8 +1,8 @@
 #!/usr/bin/env ruby
 Dir[File.dirname(__FILE__) + "/../../../lib/main.rb"].each {|file| require file }
-Dir[File.dirname(__FILE__) + "/map/*.rb"].each {|file| require file }
+Dir[File.dirname(__FILE__) + "/states/*.rb"].each {|file| require file }
 Dir[File.dirname(__FILE__) + "/users/*.rb"].each {|file| require file }
-Dir[File.dirname(__FILE__) + "/scenarios/*.rb"].each {|file| require file }
+Dir[File.dirname(__FILE__) + "/journeys/*.rb"].each {|file| require file }
 
 main = Main.new(@world)
 
