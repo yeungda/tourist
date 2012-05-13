99ded88dd37713960f1a8e4f5edf7d9c8a85f936
David Yeung <yeungda@gmail.com>
2012-05-13 22:03:53 +1000

    added a tourist command
    this replaces describe/tour/verify

--- bin/describe.rb
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env ruby
-require 'tourist'
-Dir["./states/*.rb"].each {|file| require file }
-Dir["./users/*.rb"].each {|file| require file }
-Dir["./journeys/*.rb"].each {|file| require file }
-
-main = Tourist::Main.new(@world)
-main.describe
--- bin/verify.rb
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env ruby
-require 'tourist'
-Dir["./expectations/*.rb"].each {|file| require file }
-verify
