5177d50e2b7f858f51032f1b5cb2639bf207ef3e
David Yeung <yeungda@gmail.com>
2012-06-06 22:22:30 +1000

    added blocks for expectations. could use more work

--- lib/tourist/verify.rb
+++ lib/tourist/verify.rb
@@ -8,13 +8,13 @@ def verify
   fail_count = 0
   YAML::load_documents(log) { |observation|
     @expectations.each { |expectation|
-      if expectation[:for].nil? or Tourist::StructuralMatcher.match(expectation[:for], observation)[:matches?]
-        result = Tourist::StructuralMatcher.match(expectation[:expectations], observation['observations'])
-        if result[:matches?]
-          puts "[#{observation['id']}][OK] #{expectation[:description]}"
+      if expectation.applicable?(observation)
+        result_success, result_description = expectation.assert(observation['observations'])
+        if result_success
+          puts "[#{observation['id']}][OK] #{expectation.description}"
           pass_count += 1
         else
-          puts "[#{observation['id']}]FAILED] #{expectation[:description]} because #{result[:description]}"
+          puts "[#{observation['id']}][FAILED] #{expectation.description} because #{result_description}"
           fail_count += 1
         end
       end
@@ -25,5 +25,42 @@ end
 
 def expectation(the_expectation)
   @expectations = @expectations || []
-  @expectations << the_expectation
+  @expectations << Tourist::Expectation.new(the_expectation[:description], 
+                                            the_expectation[:for],
+                                            the_expectation[:expectations],
+                                            the_expectation[:assertions])
+end
+
+class Tourist::Expectation
+  attr_reader :description
+  attr_reader :scope
+  attr_reader :structural_expectations
+
+  def initialize(description, scope, structural_expectations, assertions)
+    @description = description
+    @scope = scope
+    @structural_expectations = structural_expectations
+    @assertions = assertions
+    throw 'at least one of expectations or assertions must be provided' if @structural_expectations.nil? and @assertions.nil?
+  end
+
+  def applicable?(observation)
+    scope.nil? or Tourist::StructuralMatcher.match(scope, observation)[:matches?]
+  end
+
+  def assert(observations)
+    if not @structural_expectations.nil?
+      result = Tourist::StructuralMatcher.match(@structural_expectations, observations)
+      [result[:matches?], result[:description]]
+    elsif not @assertions.nil?
+      begin
+        @assertions.call(observations)
+        [true]
+      rescue
+        [false, $!.message + "\n" + $!.backtrace.to_s]
+      end
+    else
+      throw 'unknown expectation description'
+    end
+  end
 end
